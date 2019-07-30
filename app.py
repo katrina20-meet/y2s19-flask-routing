d9d47aeec70bacc0e7dcef5da4f8962f8b322c90
myname21-meet <myname21@meet.mit.edu>
2019-07-30 10:08:29 -0400

    flask routes

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template(
-        'home.html')
+    return render_template('home.html')
 
-@app.route('/student')
+@app.route('/student/<int:student_id>')
 def display_student(student_id):
-    return render_template('student.html')
+
+    return render_template('student.html', id_number=student_id, student=query_by_id(student_id))
+
+@app.route('/')
+def home_route():
+    return render_template('home.html')
+
 
 
 if __name__ == '__main__':
